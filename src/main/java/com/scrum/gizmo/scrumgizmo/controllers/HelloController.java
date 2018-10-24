89874d0f61d94f81f08ef95369684bd686a63c75
Punna <dharmasaiteja.p@gmail.com>
2018-10-23 23:46:57 -0500

    Added bootstrap file and basic login page

---
+++ src/main/java/com/scrum/gizmo/scrumgizmo/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.scrum.gizmo.scrumgizmo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+
+}
