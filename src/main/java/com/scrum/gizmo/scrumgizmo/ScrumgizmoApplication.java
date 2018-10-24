59f7f00d1b519b287b5719daede1a3f0306bcb8f
Punna <dharmasaiteja.p@gmail.com>
2018-10-23 21:56:18 -0500

    spring initializer base files for web app

---
+++ src/main/java/com/scrum/gizmo/scrumgizmo/ScrumgizmoApplication.java
@@ -0,0 +1,12 @@
+package com.scrum.gizmo.scrumgizmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumgizmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumgizmoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scrum/gizmo/scrumgizmo/ScrumgizmoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scrum.gizmo.scrumgizmo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScrumgizmoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
